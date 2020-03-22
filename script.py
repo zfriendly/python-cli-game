7f1515ec591ad02afecc38a3ea46139df732c17a
Zack Friendly <friendly.wisconsin@gmail.com>
2020-03-22 17:02:19 -0400

    Wrote loop to seed questions into trivia db

--- script.py
+++ script.py
@@ -15,6 +15,8 @@ class Trivia(BaseModel):
     option_2 = CharField()
     option_3 = CharField()
     answer = IntegerField()
+    category = CharField()
+    correct_count = IntegerField()
 
 
 db.connect()
@@ -29,7 +31,8 @@ trivia_data = [
         "option_2__1": "2) Minnesota Twins",
         "option_3": "3) Milwaukee Braves",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who was the first player to throw a pitch as a National?",
@@ -37,7 +40,8 @@ trivia_data = [
         "option_2__1": "2) Max Scherzer",
         "option_3": "3) Livan Hernandez",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which Nationals starter was NOT a first-round Draft choice?",
@@ -45,7 +49,8 @@ trivia_data = [
         "option_2__1": "2) Stephen Strasburg",
         "option_3": "3) Jordan Zimmermann",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who was the youngest player to win the NL Most Valuable Player award?",
@@ -53,7 +58,8 @@ trivia_data = [
         "option_2__1": "2) Bryce Harper",
         "option_3": "3) Trea Turner",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which team did the Nationals NOT face in the 2019 playoffs?",
@@ -61,7 +67,8 @@ trivia_data = [
         "option_2__1": "2) Atlanta Braves",
         "option_3": "3) Houston Astros",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which pitcher threw two no-hitters in 2015?",
@@ -69,7 +76,8 @@ trivia_data = [
         "option_2__1": "2) Gio Gonzalez",
         "option_3": "3) Stephen Strasburg",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "What year did the Nationals win their first division title?",
@@ -77,7 +85,8 @@ trivia_data = [
         "option_2__1": "2) 2008",
         "option_3": "3) 2012",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who pitched the first no-hitter in Washington Nationals history?",
@@ -85,7 +94,8 @@ trivia_data = [
         "option_2__1": "2) Livan Hernandez",
         "option_3": "3) Jordan Zimmermann",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who was the Nationals' very first draft pick?",
@@ -93,7 +103,8 @@ trivia_data = [
         "option_2__1": "2) Ryan Zimmerman",
         "option_3": "3) Terrmel Sledge",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "How many batters did Stephen Strasburg strike out in his major league debut?",
@@ -101,7 +112,8 @@ trivia_data = [
         "option_2__1": "2) 17",
         "option_3": "3) 9",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who threw the ceremonial first pitch at the grand opening of Nationals Park?",
@@ -109,7 +121,8 @@ trivia_data = [
         "option_2__1": "2) Jose Andres",
         "option_3": "3) George W. Bush",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "In what neighborhood is Nationals Park located?",
@@ -117,7 +130,8 @@ trivia_data = [
         "option_2__1": "2) Navy Yard",
         "option_3": "3) Bloomingdale",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who was the World Series MVP in 2019?",
@@ -125,7 +139,8 @@ trivia_data = [
         "option_2__1": "2) Anthony Rendon",
         "option_3": "3) Gerrit Cole",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which Nationals' pitcher has the most wins in a season?",
@@ -133,7 +148,8 @@ trivia_data = [
         "option_2__1": "2) Jordan Zimmermann",
         "option_3": "3) Gio Gonzalez",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "What season was the Capitals' first in the NHL?",
@@ -141,7 +157,8 @@ trivia_data = [
         "option_2__1": "2) 1980-81",
         "option_3": "3) 1974-75",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who is the Capitals' all-time leader in Goals?",
@@ -149,7 +166,8 @@ trivia_data = [
         "option_2__1": "2) Peter Bondra",
         "option_3": "3) Olaf Kolzig",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "How many Super Bowls have the Washington Redskins won?",
@@ -157,7 +175,8 @@ trivia_data = [
         "option_2__1": "2) Two",
         "option_3": "3) Three",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who is the Redskins all-time leading rusher?",
@@ -165,7 +184,8 @@ trivia_data = [
         "option_2__1": "2) John Riggins",
         "option_3": "3) Stephen Davis",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which Redskins' great passed away tragically in 2007?",
@@ -173,7 +193,8 @@ trivia_data = [
         "option_2__1": "2) Kirk Cousins",
         "option_3": "3) Joe Gibbs",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Who did the Capitals beat in the Stanley Cups Finals in 2017-18?",
@@ -181,7 +202,8 @@ trivia_data = [
         "option_2__1": "2) St. Louis Blues",
         "option_3": "3) Pittsburgh Penguins",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "What was the Washington Wizards' previous name?",
@@ -189,7 +211,8 @@ trivia_data = [
         "option_2__1": "2) Bullets",
         "option_3": "3) Kings",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which NBA Hall-of-Famer ended his career with the Wizards?",
@@ -197,7 +220,8 @@ trivia_data = [
         "option_2__1": "2) Vince Carter",
         "option_3": "3) Michael Jordan",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which current Wizards player was drafted #1 overall in 2010?",
@@ -205,7 +229,8 @@ trivia_data = [
         "option_2__1": "2) John Wall",
         "option_3": "3) Thomas Bryant",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "In which league does D.C. United play?",
@@ -213,7 +238,8 @@ trivia_data = [
         "option_2__1": "2) MLS",
         "option_3": "3) NBA",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "The Capital City Go-Go are an affiliate of which D.C. sports team?",
@@ -221,7 +247,8 @@ trivia_data = [
         "option_2__1": "2) Capitals",
         "option_3": "3) Nationals",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Ted Leonsis owns two D.C. sports franchises. Which two does he own?",
@@ -229,7 +256,8 @@ trivia_data = [
         "option_2__1": "2) Redskins & Nationals",
         "option_3": "3) Capitals and Wizards",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Which former Redskins QB was the first black QB to win a Super Bowl?",
@@ -237,7 +265,8 @@ trivia_data = [
         "option_2__1": "2) Doug Williams",
         "option_3": "3) Robert Griffin III",
         "answer": 2,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Three of the four major franchises in D.C. use the same color scheme (Red, White and Blue). Which team does not?",
@@ -245,7 +274,8 @@ trivia_data = [
         "option_2__1": "2) Wizards",
         "option_3": "3) Nationals",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "What was the name of the Washington baseball franchise before the Nationals?",
@@ -253,7 +283,8 @@ trivia_data = [
         "option_2__1": "2) Congressmen",
         "option_3": "3) Senators",
         "answer": 3,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "Ranking second all time in NHL history, how many 40-goal seasons does Alex Ovechkin have?",
@@ -261,7 +292,8 @@ trivia_data = [
         "option_2__1": "2) 8",
         "option_3": "13) 12",
         "answer": 1,
-        "category": 1
+        "category": "sports",
+        "correct_count": 0
     },
     {
         "question": "What is the population of Washington, DC as of 2019?",
@@ -269,7 +301,8 @@ trivia_data = [
         "option_2__1": "2) 711,571",
         "option_3": "3) 800, 331",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "How many senators does Washington, DC.have?",
@@ -277,7 +310,8 @@ trivia_data = [
         "option_2__1": "2) Two",
         "option_3": "3) Zero",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "What two states border Washington, DC?",
@@ -285,7 +319,8 @@ trivia_data = [
         "option_2__1": "2) MD & WV",
         "option_3": "3) VA & MD",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "What is the street address of the White House?",
@@ -293,7 +328,8 @@ trivia_data = [
         "option_2__1": "2) 123 Capital St.",
         "option_3": "3) 1000 North Capital St.",
         "answer": 1,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "In which war were the Capital, Treasure, and White House burned down?",
@@ -301,7 +337,8 @@ trivia_data = [
         "option_2__1": "2) War of 1812",
         "option_3": "3) World War I",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which educational foundation maintains most museums in DC and keeps them free?",
@@ -309,7 +346,8 @@ trivia_data = [
         "option_2__1": "2) Macarthur",
         "option_3": "3) Smithsonian",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "The ______ Center for Performing Arts is named after which former President?",
@@ -317,7 +355,8 @@ trivia_data = [
         "option_2__1": "2) Adams",
         "option_3": "3) Eisenhower",
         "answer": 1,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which university in Washington shares a name with the neighborhood in which it is located?",
@@ -325,7 +364,8 @@ trivia_data = [
         "option_2__1": "2) Gallaudet University",
         "option_3": "3) Georgetown University",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "The National ___ houses many of DC's museums and institutions.",
@@ -333,7 +373,8 @@ trivia_data = [
         "option_2__1": "2) Mall",
         "option_3": "3) Monument",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "How tall is the Washington Monument?",
@@ -341,7 +382,8 @@ trivia_data = [
         "option_2__1": "2) 555 ft.",
         "option_3": "3) 586 ft.",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Who was the first president to live in the White House?",
@@ -349,7 +391,8 @@ trivia_data = [
         "option_2__1": "2) George Washington",
         "option_3": "3) John F. Kennedy",
         "answer": 1,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "The streets in D.C. that run diagonally are named after ____.",
@@ -357,7 +400,8 @@ trivia_data = [
         "option_2__1": "2) Words",
         "option_3": "3) States",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which genre of music (combining hip-hop and live band music) originated in DC?",
@@ -365,7 +409,8 @@ trivia_data = [
         "option_2__1": "2) Go-Go",
         "option_3": "3) Emo",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which \"letter\" street is missing in Washington, DC?",
@@ -373,7 +418,8 @@ trivia_data = [
         "option_2__1": "2) M",
         "option_3": "3) J",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which famous sci-fi movie character is featured as a gargoyle on the National Cathedral?",
@@ -381,7 +427,8 @@ trivia_data = [
         "option_2__1": "2) Darth Vader",
         "option_3": "3) Frodo Baggins",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "What institution in the DC area has 17 miles of corridors?",
@@ -389,7 +436,8 @@ trivia_data = [
         "option_2__1": "2) National Archives",
         "option_3": "3) Pentagon",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "What DC institution houses the United States Constitution & Declaration of Independence?",
@@ -397,7 +445,8 @@ trivia_data = [
         "option_2__1": "2) Library of Congress",
         "option_3": "1) White House",
         "answer": 1,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "How many bathrooms are in the White House?",
@@ -405,7 +454,8 @@ trivia_data = [
         "option_2__1": "2) 10",
         "option_3": "3) 55",
         "answer": 1,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "The Reflecting Pool sits between the Washington Monument and The ____.",
@@ -413,7 +463,8 @@ trivia_data = [
         "option_2__1": "2) Vietnam Memorial",
         "option_3": "3) Lincoln Memorial",
         "answer": 3,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     },
     {
         "question": "Which neighborhood, dating back to 1751, is the oldest part of the city?",
@@ -421,6 +472,13 @@ trivia_data = [
         "option_2__1": "2) Georgetown",
         "option_3": "3) Foggy Bottom",
         "answer": 2,
-        "category": 2
+        "category": "general",
+        "correct_count": 0
     }
 ]
+
+
+for triv in trivia_data:
+    triv = Trivia(question=triv['question'], option_1=triv['option_1'], option_2=triv['option_2'],
+                  option_3=triv['option_3'], answer=triv['answer'], category=triv['category'], correct_count=0)
+    triv.save()
